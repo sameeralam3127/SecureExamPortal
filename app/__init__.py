fc02fa1e4bf915ba213973332e782848ad15b807
SAMEER ALAM <sameeralam3127@gmail.com>
2025-11-28 17:22:55 +0530

    Removed old code base

--- app/__init__.py
+++
@@ -1,4 +0,0 @@
-from .auth.routes import auth_bp
-app.register_blueprint(auth_bp)
-from flask_talisman import Talisman
-Talisman(app)
--- tests/test_basic.py
+++
@@ -1,5 +0,0 @@
-def test_homepage(client):
-    response = client.get("/")
-    
-    assert response.status_code == 200
-    assert b"Exam" in response.data
